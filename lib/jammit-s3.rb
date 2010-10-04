7259d6e223556b312ff81a1e55fce5058731201a
Jacques Crocker <railsjedi@gmail.com>
2010-10-04 15:11:58 -0700

    Set asset_host via Railtie

--- lib/jammit-s3.rb
+++ lib/jammit-s3.rb
@@ -7,3 +7,18 @@ module Jammit
     S3Uploader.new(options).upload
   end
 end
+
+if defined?(Rails)
+  module Jammit
+    class JammitRailtie < Rails::Railtie
+      initializer "set asset host and asset id" do
+        config.after_initialize do
+          s3_bucket = Jammit.configuration[:s3_bucket]
+          if Jammit.package_assets and s3_bucket.present?
+            ActionController::Base.asset_host = "http://#{Jammit.configuration[:s3_bucket]}.s3.amazonaws.com"
+          end
+        end
+      end
+    end
+  end
+end
