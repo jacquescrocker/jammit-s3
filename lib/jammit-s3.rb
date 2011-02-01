d1c4ffde0e563d90659e541b249c46307913e6c0
Peter Dedene <peter@dedene.com>
2011-02-01 20:13:56 +0100

    Added Cloudfront support in config file

--- lib/jammit-s3.rb
+++ lib/jammit-s3.rb
@@ -7,3 +7,37 @@ module Jammit
     S3Uploader.new(options).upload
   end
 end
+
+if defined?(Rails)
+  module Jammit
+    class JammitRailtie < Rails::Railtie
+      initializer "set asset host and asset id" do
+        config.after_initialize do
+          if Jammit.configuration[:use_cloudfront] && Jammit.configuration[:cloudfront_cname].present? && Jammit.configuration[:cloudfront_domain].present?
+            asset_hostname = Jammit.configuration[:cloudfront_cname]
+            asset_hostname_ssl = Jammit.configuration[:cloudfront_domain]
+          elsif Jammit.configuration[:use_cloudfront] && Jammit.configuration[:cloudfront_domain].present?
+            asset_hostname = asset_hostname_ssl = Jammit.configuration[:cloudfront_domain]            
+          else
+            asset_hostname = asset_hostname_ssl = "#{Jammit.configuration[:s3_bucket]}.s3.amazonaws.com"
+          end
+
+          if Jammit.package_assets and asset_hostname.present?
+            ActionController::Base.asset_host = Proc.new do |source, request|
+              if Jammit.configuration.has_key?(:ssl)
+                protocol = Jammit.configuration[:ssl] ? "https://" : "http://"
+              else
+                protocol = request.protocol
+              end
+              if request.protocol == "https://"
+                return "#{protocol}#{asset_hostname_ssl}"
+              else 
+                return "#{protocol}#{asset_hostname}"  
+              end              
+            end
+          end
+        end
+      end
+    end
+  end
+end
